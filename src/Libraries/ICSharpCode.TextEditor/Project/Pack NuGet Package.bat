41e4ed5fa6eeb2b1aaca1b35196df62d714b88dc
Pieter van Ginkel <pvginkel@gmail.com>
2014-11-30 12:17:48 +0100

    Created version 3.3.5.6482.

--- src/Libraries/ICSharpCode.TextEditor/Project/Pack NuGet Package.bat
+++ src/Libraries/ICSharpCode.TextEditor/Project/Pack NuGet Package.bat
@@ -1,3 +1,3 @@
 @echo off
 
-nuget.exe pack -build -prop configuration=release -version 3.3.4.6480 NetIde.ICSharpCode.TextEditor.nuspec
+nuget.exe pack -build -prop configuration=release -version 3.3.5.6482 NetIde.ICSharpCode.TextEditor.nuspec
